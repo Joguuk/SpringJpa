e48d2859c86d71b0b6c7c8e4bcd0ef27a135fb4e
joguuk <jjozerg@gmail.com>
2021-05-06 12:33:09 +0900

    jpa shop complete

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -40,7 +41,7 @@ public class ItemController {
 
     @GetMapping(value = "items")
     public String list(Model model) {
-        List<Item> items = itemService.findItem();
+        List<Item> items = itemService.findItems();
 
         model.addAttribute("items", items);
 
@@ -61,8 +62,20 @@ public class ItemController {
 
         model.addAttribute("form", form);
         return "items/updateItemForm";
+    }
 
+    @PostMapping(value = "items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+        Book book = new Book();
 
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
 
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
---
+++ jpashop/src/test/java/jpabook/jpashop/ItemUpdateTest.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() {
+//        Book book = em.find();
+
+
+    }
+}
