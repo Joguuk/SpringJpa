3681e924e488f29849c9cf7a86f3f5579131348e
joguuk <jjozerg@gmail.com>
2021-05-23 10:15:22 +0900

    lazy loading과 성능 최적화(fetch join, DTO select)

--- jpashop-api/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop-api/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -7,7 +7,6 @@ import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @Repository
@@ -69,5 +68,13 @@ public class OrderRepository {
             query = query.setParameter("name", orderSearch.getMemberName());
         }
         return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
     }
 }
