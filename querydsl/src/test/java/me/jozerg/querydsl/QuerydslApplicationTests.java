ded0a08bf204a18b54d48b978fb03230558a3517
joguuk <jjozerg@gmail.com>
2021-10-16 14:42:33 +0900

    feat: querydsl 세팅
    1. querydsl 설정
    - build.gradle 추가

---
+++ querydsl/src/test/java/me/jozerg/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,37 @@
+package me.jozerg.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.jozerg.querydsl.entity.Hello;
+import me.jozerg.querydsl.entity.QHello;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = queryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+}
