4ceb3ee998e2dd0d3e2add13125107d1d2e1cc70
joguuk <jjozerg@gmail.com>
2022-01-03 01:10:58 +0900

    <querydsl> : dynamic where clause
    1. BooleanExpression을 사용해서 where절 조합
      - 재사용이 가능해서 BooleanBuilder보다 장점이 있음!

--- querydsl/src/main/java/me/jozerg/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/me/jozerg/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package me.jozerg.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.jozerg.querydsl.dto.MemberSearchCondition;
 import me.jozerg.querydsl.dto.MemberTeamDto;
@@ -87,5 +89,63 @@ public class MemberJpaRepository {
                 .leftJoin(member.team, team)
                 .where(booleanBuilder)
                 .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    // 1. where 조건에 BooleanExpression을 사용하면 재사용이 가능하기 때문에 권장.
+    // 2. 조합이 가능(이것도 재사용이긴 하지??)
+    public List<Member> searchMember(MemberSearchCondition condition) {
+        return queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageBetween(condition.getAgeLoe(), condition.getAgeGoe())
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    // null check를 보완해야겠지만 이것도 가능
+    private BooleanExpression ageBetween(int ageLoe, int ageGoe) {
+        return ageLoe(ageLoe).and(ageGoe(ageGoe));
+    }
+
+    // Predicate 보다 BooleanExpression이 좋음
+    // 왜냐하면 BooleanExpression은 Conposite 조합이 가능하기 때문
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 }
